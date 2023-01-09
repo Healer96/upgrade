adf360fabfcd88e27a6d523d09da9394209df8b5
Powergold8 <114437993+Powergold8@users.noreply.github.com>
2023-01-09 18:45:00 +0100

    Add files via upload

---
+++ MonsterStomp.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterStomp : MonoBehaviour
+{
+    private void OnColiisionEnter2D(Collision2D collision) {
+        if(collision.gameObject.tag == "Weak Point") {
+            Destroy(collision.gameObject);
+        }
+    }
+
+}
